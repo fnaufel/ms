b66fd3be06442bb42e0d25a2bab3c24ce4458aae
fnaufel <fnaufel@gmail.com>
2021-01-25 19:53:51 -0300

    Delete repeated lines

--- R/ler_resultados.R
+++ R/ler_resultados.R
@@ -11,9 +11,6 @@
 ler_resultados <- function(nome) {
 
   lido <- read_html(nome, encoding = 'UTF-8')
-  lido <- read_html(nome, encoding = 'UTF-8')
-  lido <- read_html(nome, encoding = 'UTF-8')
-  lido <- read_html(nome, encoding = 'UTF-8')
   
   lista <- lido %>% 
     rvest::html_table(
